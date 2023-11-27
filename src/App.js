aaa176318ae8bb4ffd33eb4ca35acfd25c2abd03
altynbeq <starknbek@gmail.com>
2023-11-27 22:47:01 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Alert from './components/Alert.jsx';
+import List from './components/List';
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show:false, msg:'', type:''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!name){
+    } else if(name && isEditing){
+    } else {
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      }
+      setList([...list, newItem]);
+      setName('');
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input type="text" placeholder='add items..' className='grocery' value={name} 
+            onChange={(e) => setName(e.target.value)}>
+          </input>
+          <button type='submit' className='submit-btn'>{isEditing ? 'Submit' : 'Edit'}</button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List items={list}/>
+      <button type='submit' className='clear-btn'>Remove items</button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/List.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const List = ({items}) => {
+    return(
+        <div>
+            {items.map((item) => {
+                const {id, title} = item; 
+                return(
+                    <article key={id} className='grocery-item'>
+                        <p className="title">{title}</p>
+                        <div className="btn-container">
+                            <button className="edit-btn" type="button">
+                                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                                <i class="fa fa-pencil-square-o" style={{fontSize: "15px"}} aria-hidden="true"></i>
+                            </button>
+                            <button className="delete-btn" type="button">
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                                <i class="fa fa-trash-o" style={{fontSize: "15px"}} aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </article>
+                )
+            })}
+        </div>
+    )
+};
+
+export default List;
